1bc74494eb22b867148480d5845ea42203fcbb34
CodeRumbles <coderumble01@gmail.com>
2025-04-04 19:51:10 +0530

    3

--- keylogserver/settings.py
+++ keylogserver/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from pathlib import Path
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')  # fallback if no env var
+}
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
